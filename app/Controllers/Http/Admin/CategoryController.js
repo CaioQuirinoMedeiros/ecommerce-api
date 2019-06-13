e6911d4980320daf17dfd9d8ca2dbe352bbcf320
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-13 20:30:57 -0300

    CategoryController shwo and destroy

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -17,6 +17,34 @@ class CategoryController {
 
     return response.status(200).send(categories)
   }
+
+  async store({ request, response }) {
+    try {
+      const { title, description, image_id } = request.all()
+
+      const category = await Category.create({ title, description, image_id })
+
+      return response.status(201).send({ category })
+    } catch (err) {
+      return response
+        .status(400)
+        .send({ message: 'Erro ao processar solicitação' })
+    }
+  }
+
+  async show({ params, response }) {
+    const category = await Category.findOrFail(params.id)
+
+    return response.send(category)
+  }
+
+  async destroy({ params, response }) {
+    const category = await Category.findOrFail(params.id)
+
+    await category.delete()
+
+    return response.status(204).send()
+  }
 }
 
 module.exports = CategoryController
