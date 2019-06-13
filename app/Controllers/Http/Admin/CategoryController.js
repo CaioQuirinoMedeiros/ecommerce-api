62e2a3f58e3edbeebfaf8f81bfcb827266c595fe
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-13 20:23:23 -0300

    CategoryController index

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -1,6 +1,22 @@
 'use strict'
 
+const Category = use('App/Models/Category')
+
 class CategoryController {
+  async index({ request, response, pagination }) {
+    const title = request.input('title')
+    const { page, limit } = pagination
+
+    const query = Category.query()
+
+    if (title) {
+      query.where('title', 'iLIKE', `%${title}%`)
+    }
+
+    const categories = await query.paginate(page, limit)
+
+    return response.status(200).send(categories)
+  }
 }
 
 module.exports = CategoryController
