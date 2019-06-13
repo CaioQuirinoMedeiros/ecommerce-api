2206d9077e506f963989e87be9bbfbcc0aa6aaa6
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-13 17:28:23 -0300

    AuthController register and login

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,9 +1,39 @@
 'use strict'
 
-class AuthController {
-  async register({ request, response }) {}
+const Database = use('Database')
+const User = use('App/Models/User')
+const Role = use('Role')
 
-  async login({ request, response, auth }) {}
+class AuthController {
+  async register({ request, response }) {
+    const trx = Database.beginTransaction()
+
+    try {
+      const { name, surname, email, password } = request.all()
+
+      const user = await User.create({ name, surname, email, password }, trx)
+
+      const clientRole = await Role.findBy('slug', 'client')
+
+      await user.roles().attach([clientRole.id], null, trx)
+
+      await trx.commit()
+
+      return user
+    } catch (err) {
+      await trx.rollback()
+
+      return response.status(400).send({ message: 'Erro ao realizar cadastro' })
+    }
+  }
+
+  async login({ request, response, auth }) {
+    const { email, password } = request.all()
+
+    const token = await auth.withRefreshToken().attempt(email, password)
+
+    return token
+  }
 
   async refresh({ request, response, auth }) {}
 
