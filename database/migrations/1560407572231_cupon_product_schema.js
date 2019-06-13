5946d5441b28c8757943116dafaafb01c657ffdf
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-13 03:33:54 -0300

    cupon_product migration

---
+++ database/migrations/1560407572231_cupon_product_schema.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CuponProductSchema extends Schema {
+  up() {
+    this.create('cupon_product', table => {
+      table.increments()
+      table.integer('cupon_id').unsigned()
+      table.integer('user_id').unsigned()
+      table.timestamps()
+
+      table
+        .foreign('cupon_id')
+        .references('id')
+        .inTable('cupons')
+        .onDelete('cascade')
+      table
+        .foreign('product_id')
+        .references('id')
+        .inTable('products')
+        .onDelete('cascade')
+    })
+  }
+
+  down() {
+    this.drop('cupon_product')
+  }
+}
+
+module.exports = CuponProductSchema
