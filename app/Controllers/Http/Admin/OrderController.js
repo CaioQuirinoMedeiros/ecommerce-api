c8a447854832df368dc35af373ff5f3a1d3155c9
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-14 16:45:27 -0300

    OrderController

--- app/Controllers/Http/Admin/OrderController.js
+++ app/Controllers/Http/Admin/OrderController.js
@@ -3,48 +3,144 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 
+const Order = use('App/Models/Order')
+const Databse = use('Database')
+const Service = use('App/Services/Order/OrderService')
+
 class OrderController {
   /**
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
+   * @param {object} ctx.pagination
    */
-  async index({ request, response, view }) {}
+  async index({ request, response, pagination }) {
+    const { page, limit } = pagination
+    const status = request.input('status')
+
+    const query = Order.query()
+
+    if (status) {
+      query.where('status', status)
+    }
+
+    const orders = await Order.query()
+      .orderBy('id', 'DESC')
+      .paginate(page, limit)
+
+    return response.status(200).send(orders)
+  }
 
   /**
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async create({ request, response, view }) {}
+  async store({ request, response }) {
+    const trx = await Database.beginTransaction()
 
-  /**
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store({ request, response }) {}
+    try {
+      const { user_id, items, status } = request.all()
+
+      const order = await Order.create({
+        user_id,
+        items,
+        status,
+        trx
+      })
+
+      const service = new Service(order, trx)
+
+      if (items && items.length) {
+        await service.syncItems(items)
+      }
+
+      await trx.commit()
+
+      return response.status(201).send(order)
+    } catch (err) {
+      await trx.rollback()
+
+      return response
+        .status(400)
+        .send({ message: 'Não foi possível criar o pedido' })
+    }
+  }
 
   /**
    * @param {object} ctx
    * @param {Params} ctx.params
    * @param {Response} ctx.response
+   * @param {object} ctx.params
    */
-  async show({ params, request, response, view }) {}
+  async show({ params, response }) {
+    const order = await Order.findOrFail(params.id)
+
+    await order.load('user')
+
+    return response.send(order)
+  }
 
   /**
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
+   * @param {object} ctx.params
    */
-  async update({ params, request, response }) {}
+  async update({ params, request, response }) {
+    const order = await Order.findOrFail(params.id)
+
+    const trx = await Database.beginTransaction()
+
+    try {
+      const { user_id, items, status } = request.all()
+
+      order.merge({ user_id, status })
+
+      const service = new Service(order, trx)
+
+      await service.updateItems(items)
+
+      await order.save(trx)
+
+      await trx.commit()
+
+      return response.status(200).send(order)
+    } catch (err) {
+      await trx.rollback()
+
+      return response
+        .status(400)
+        .send({ message: 'Não foi possível atualizar o pedido' })
+    }
+  }
 
   /**
    * @param {object} ctx
-   * @param {Request} ctx.request
+   * @param {object} ctx.params
    * @param {Response} ctx.response
    */
-  async destroy({ params, request, response }) {}
+  async destroy({ params, response }) {
+    const order = await Order.findOrFail(params.id)
+
+    const trx = await Database.beginTransaction()
+
+    try {
+      await order.items().delete(trx)
+      await order.cupons().delete(trx)
+      await order.delete(trx)
+
+      await trx.commit()
+
+      return response.status(204).send()
+    } catch (err) {
+      await trx.rollback()
+
+      return response
+        .status(500)
+        .send({ message: 'Não foi possível deletar o pedido' })
+    }
+  }
 }
 
 module.exports = OrderController
