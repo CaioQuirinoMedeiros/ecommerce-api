09f61fae8e00db9941c8243c9e985fc12c0b8938
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-14 03:31:53 -0300

    Helpers manage_single file/ manage_multiple_files

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -30,7 +30,51 @@ const random_string = async (length = 40) => {
  * then 'public/uploads' will be used
  * @param {FileJar} file the file to be managed
  * @param {string} path the path where the file should be moved in
+ * @return {Object<FileJar>}
  */
-const manage_single_upload = async (file, path = null) => {}
+const manage_single_upload = async (file, path = null) => {
+  path = path ? path : Helpers.publicPath('uploads')
+
+  const random_name = await random_string(30)
+
+  const filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+  await file.move(path, {
+    name: filename
+  })
+}
+
+/**
+ * Move multiple files to a specified path, if no path is specified
+ * then 'public/uploads' will be used
+ * @param {FileJar} fileJar the files to be managed
+ * @param {string} path the path where the file should be moved in
+ * @return {Object}
+ */
+const manage_multiple_files = async (fileJar, path = null) => {
+  path = path ? path : Helpers.publicPath('uploads')
+
+  let success = []
+  let erros = []
+
+  await Promise.all(
+    fileJar.files.map(async file => {
+      const random_name = await random_string(30)
+      const filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+      await file.move(path, {
+        name: filename
+      })
+
+      if (file.moved()) {
+        success.push(file)
+      } else {
+        erros.push(file.error())
+      }
+    })
+  )
+
+  return { success, errors }
+}
 
 module.exports = { randomString }
