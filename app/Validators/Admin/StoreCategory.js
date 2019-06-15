e211ffc056761471542400b5abc32721ba827f02
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-15 11:13:35 -0300

    Improving Category validator

--- app/Validators/Admin/StoreCategory.js
+++ app/Validators/Admin/StoreCategory.js
@@ -7,15 +7,17 @@ class StoreCategory {
 
   get rules() {
     return {
-      title: 'required',
-      description: 'required'
+      title: 'required|string',
+      description: 'required|string'
     }
   }
 
   get messages() {
     return {
-      'title.required': 'O título é obrigatório',
-      'description.required': 'A descrição é obrigatória'
+      'title.required': 'O título é obrigatório!',
+      'title.string': 'O título tem que ser um texto!',
+      'description.required': 'A descrição é obrigatória!',
+      'description.string': 'A descrição tem que ser um texto!'
     }
   }
 }
