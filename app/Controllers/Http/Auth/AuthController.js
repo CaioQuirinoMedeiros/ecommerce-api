9003e866d24736c286018305cf36b7860e78f98f
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-15 00:50:57 -0300

    Standartizing response.send

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -19,7 +19,7 @@ class AuthController {
 
       await trx.commit()
 
-      return response.status(201).send({ data: user })
+      return response.status(201).send(user)
     } catch (err) {
       await trx.rollback()
 
@@ -32,7 +32,7 @@ class AuthController {
 
     const token = await auth.withRefreshToken().attempt(email, password)
 
-    return response.send({ data: token })
+    return response.send(token)
   }
 
   async refresh({ request, response, auth }) {
@@ -46,7 +46,7 @@ class AuthController {
       .newRefreshToken()
       .generateForRefreshToken(refresh_token)
 
-    return response.send({ data: user })
+    return response.send(user)
   }
 
   async logout({ request, response, auth }) {
@@ -58,7 +58,7 @@ class AuthController {
 
     await auth.authenticator('jwt').revokeTokens(['refresh_token'], true)
 
-    return response.status(204).send({})
+    return response.status(204).send()
   }
 
   async forgot({ request, response }) {}
