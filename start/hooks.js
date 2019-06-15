d2f771f9fe7d5e87323b6284d053740631eac9f7
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-14 23:43:39 -0300

    Hook to check value in database

---
+++ start/hooks.js
@@ -0,0 +1,26 @@
+const { hooks } = require('@adonisjs/fold')
+
+const Validator = use('Validator')
+const Database = use('Database')
+
+hooks.after.providersBooted(() => {
+  const existsFn = async (data, field, message, args, get) => {
+    const value = get(data, field)
+    if (!value) {
+      /**
+       * skip validation if value is not defined. `required` rule
+       * should take care of it.
+       */
+      return
+    }
+
+    const [table, column] = args
+    const row = await Database.table(table)
+      .where(column, value)
+      .first()
+
+    if (!row) {
+      throw message
+    }
+  }
+})
