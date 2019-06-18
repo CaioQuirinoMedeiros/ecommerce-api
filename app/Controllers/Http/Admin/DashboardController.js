d23f612dba2d62bcc30cc15d7c45e26a5ce7714c
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-18 11:04:49 -0300

    Bug fixed

--- app/Controllers/Http/Admin/DashboardController.js
+++ app/Controllers/Http/Admin/DashboardController.js
@@ -4,16 +4,23 @@ const Database = use('Database')
 
 class DashboardController {
   async index({ response }) {
-    const users = await Dabatase.from('users').getCount()
-    const orders = await Dabatase.from('orders').getCount()
-    const products = await Dabatase.from('products').getCount()
+    try {
+      const users = await Database.from('users').getCount()
+      const orders = await Database.from('orders').getCount()
+      const products = await Database.from('products').getCount()
 
-    const subtotal = await Database.from('order_items').getSum('subtotal')
-    const discounts = await Database.from('cupon_order').getSum('discount')
+      const subtotal = await Database.from('order_items').getSum('subtotal')
+      const discounts = await Database.from('cupon_order').getSum('discount')
 
-    const revenue = subtotal - discounts
+      const revenue = subtotal - discounts
 
-    response.status(200).send({ users, orders, products, revenue })
+      response.status(200).send({ users, orders, products, revenue })
+    } catch (err) {
+      console.log(err)
+      return response
+        .status(400)
+        .send({ message: 'Não foi possível caregar o dashboard' })
+    }
   }
 }
 
